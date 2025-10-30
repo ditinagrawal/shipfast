ad50fb85900b5356cba99bd3b0f7b9e3c7cf3ab1
ditinagrawal <ditin.agrawal05@gmail.com>
2025-10-30 15:06:57 +0530

    setup better auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { Toaster } from "react-hot-toast";
+
 import "./globals.css";
 
 const inter = Inter({
@@ -19,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
